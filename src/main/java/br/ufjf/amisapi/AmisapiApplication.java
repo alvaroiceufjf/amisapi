95022d83b1ec5f684d70d4f8cb702d48c5234ca2
alvaroiceufjf <alvaro.domingues@ufjf.br>
2026-04-28 22:04:56 -0300

    cria projeto

---
+++ src/main/java/br/ufjf/amisapi/AmisapiApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.amisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmisapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmisapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufjf/amisapi/AmisapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.amisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmisapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
